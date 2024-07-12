067cbb51d8974a95eb960a04aad0e96214d83161
Rahim <rahimgangji428@gmail.com>
2024-07-12 12:29:36 -0500

    changes

--- src/components/teams-com/single-team.jsx
+++ src/components/teams-com/single-team.jsx
@@ -1,30 +1,34 @@
-import React from 'react';
+import React from "react";
 
-const SingleTeam = ({team}) => {
-  return (
-    <div className="single-team-member">
-      <div className="wrapper">
-        <div className="img-box">
-          <img src={team.img} alt="" />
+const SingleTeam = ({ team }) => {
+    return (
+        <div className="single-team-member ">
+            <div className="wrapper">
+                <div className="img-box ">
+                    <img src={team.img} alt="" className="" />
+                </div>
+                <div className="info-meta">
+                    <h6 className="name">{team.name}</h6>
+                    <span>{team.title}</span>
+                </div>
+            </div>
+            <div className="hover-content">
+                <ul>
+                    {team.social_links.map((link, i) => (
+                        <li key={i}>
+                            <a
+                                href={link.link}
+                                target={"_blank"}
+                                rel="noreferrer"
+                            >
+                                <i className={link.icon} aria-hidden="true"></i>
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
-        <div className="info-meta">
-          <h6 className="name">{team.name}</h6>
-          <span>{team.title}</span>
-        </div>
-      </div>
-      <div className="hover-content">
-        <ul>
-          {team.social_links.map((link, i) => (
-            <li key={i}>
-              <a href={link.link} target={"_blank"} rel="noreferrer">
-                <i className={link.icon} aria-hidden="true"></i>
-              </a>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default SingleTeam;
